5045c79d9fa91cf7c8e4ad1e54e6d34567a6cd87
samhainsamhainsamhain <83346225+samhainsamhainsamhain@users.noreply.github.com>
2022-04-24 12:03:34 +0300

    added quick sorting algorithm

--- src/Algorithms/BubbleSort.ts
+++ src/Algorithms/BubbleSort.ts
@@ -1,16 +1,16 @@
 import { swap } from "./Utility";
 
 export default function BubbleSort(array: number[]) {
-    let newArray: number[] = [...array];
-    const newArraySteps = [];
-    for (let i = 0; i < newArray.length - 1; i++) {
-      for (let j = 0; j < newArray.length - i - 1; j++) {
-        if (newArray[j] > newArray[j + 1]) {
-          newArray = swap(newArray, j, j + 1);
-        }
-        newArraySteps.push(newArray.slice());
+  let newArray: number[] = [...array];
+  const newArraySteps = [];
+  for (let i = 0; i < newArray.length - 1; i++) {
+    for (let j = 0; j < newArray.length - i - 1; j++) {
+      if (newArray[j] > newArray[j + 1]) {
+        newArray = swap(newArray, j, j + 1);
       }
       newArraySteps.push(newArray.slice());
     }
-    return newArraySteps;
+    newArraySteps.push(newArray.slice());
   }
+  return newArraySteps;
+}
