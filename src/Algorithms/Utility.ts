d002ca72e866c27fd3c55542e5cb1ab3084734a4
samhainsamhainsamhain <83346225+samhainsamhainsamhain@users.noreply.github.com>
2022-04-24 12:17:57 +0300

    added sorting based on luck

--- src/Algorithms/Utility.ts
+++ src/Algorithms/Utility.ts
@@ -4,3 +4,31 @@ export function swap(array: number[], a: number, b: number) {
   array[b] = temp;
   return array;
 }
+
+export function shuffle(array: number[]) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
+
+export function arrayIsEqual(array: number[], sortedArray: number[]) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== sortedArray[i]) {
+      return false;
+    }
+  }
+  return true;
+}
